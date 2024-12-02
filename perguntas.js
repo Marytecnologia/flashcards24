10de87c3e099a8cd8911b9dbca2bffe9846b9b7b
Marytecnologia <00001093102111SP@al.educacao.sp.gov.br>
2024-12-02 08:18:40 -0300

    flashmary

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual o ano em que Getúlio Vargas morreu?',
+    'Getúlio Vargas morreu no ano de 1954'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do México?',
+    'A capital do México é Cidade do México (México City)'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual a escola literária que caracteriza-se pelos detalhes, pelo exagero e pelo rebuscamento?',
+    'Barroco (1601-1768)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz nariz em Inglês?',
+    'Nariz em ingles é NOSE'
+)
